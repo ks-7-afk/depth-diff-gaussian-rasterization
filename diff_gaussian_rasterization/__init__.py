d668f5a4b63cd5bcf03358d0745c18f7f61e4573
ks-7-afk <knlebedeva@stud.etu.ru>
2024-02-05 15:36:52 +0300

    train fixed

--- diff_gaussian_rasterization/__init__.py
+++ diff_gaussian_rasterization/__init__.py
@@ -98,7 +98,7 @@ class _RasterizeGaussians(torch.autograd.Function):
         return color, radii, depth, xyz
 
     @staticmethod
-    def backward(ctx, grad_out_color, grad_radii, grad_depth):
+    def backward(ctx, grad_out_color, grad_radii, grad_depth, xyz):
 
         # Restore necessary values from context
         num_rendered = ctx.num_rendered
